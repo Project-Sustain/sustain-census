d4fa4caea6c09fe4e658929e1466ab6e0a4a14a7
Caleb Carlson <ccarlson355@gmail.com>
2021-01-21 14:46:39 -0700

    feat(core): Test11

--- src/main/scala/org/sustain/SparkModel.scala
+++ src/main/scala/org/sustain/SparkModel.scala
@@ -1,5 +1,19 @@
-package org.sustain
+/* -----------------------------------------------
+ * SparkModel.scala -
+ *
+ * Description:
+ *    Provides a demonstration of the Spark MongoDB Connector, by
+ *    implementing a linear regression model on one of the SUSTAIN
+ *    datasets in the MongoDB sharded cluster.
+ *    Guide for this project taken directly from MongoDB docs:
+ *    https://docs.mongodb.com/spark-connector/master/scala-api
+ *
+ *  Author:
+ *    Caleb Carlson
+ *
+ * ----------------------------------------------- */
 
+package org.sustain
 
 // Main entrypoint for the Spark Model that gets submitted to the cluster.
 object SparkModel {
@@ -24,6 +38,57 @@ object SparkModel {
         println("Caught IllegalArgumentException:\n" + ex.getMessage)
       }
     }
+
+    /* Minimum Imports */
+    import com.mongodb.spark.config.ReadConfig
+    import com.mongodb.spark.sql.DefaultSource
+    import com.mongodb.spark.MongoSpark
+    import org.apache.spark.sql.SparkSession
+    import org.apache.spark.sql.DataFrame
+    import org.apache.spark.sql.Dataset
+    import org.apache.spark.sql.Row
+    import org.apache.spark.sql.functions.col
+    import org.apache.spark.ml.feature.{VectorAssembler, StringIndexer}
+    import org.apache.spark.ml.regression.LinearRegression
+    import org.apache.spark.ml.regression.LinearRegressionModel
+    import org.apache.spark.ml.evaluation.RegressionEvaluator
+
+
+    // Create the SparkSession and ReadConfig
+    val sparkConnector: SparkSession = SparkSession.builder()
+      .master(configuration("sparkMaster"))
+      .appName("SustainConnector")
+      .config("spark.mongodb.input.uri", configuration("databaseHost"))
+      .config("spark.mongodb.input.database", configuration("databaseName"))
+      .config("spark.mongodb.input.collection", configuration("collection"))
+      .getOrCreate()
+
+    import sparkConnector.implicits._ // For the $()-referenced columns
+
+    // Read collection into a DataFrame
+    val df: DataFrame = MongoSpark.load(sparkConnector)
+
+    // Only select relevant fields
+    val df1: DataFrame = df.select("GISJOIN", "_id", "temp", "year")
+
+    val gisJoins: Dataset[Row] = df.select("GISJOIN").distinct()
+
+    for (gisJoinRow: Row <- gisJoins.collect()) {
+      val gisJoin: String = gisJoinRow.getString(0)
+
+      println(gisJoin)
+    }
+
+    // Group by GISJOIN and year, selecting the max of year
+    //val ds2: Dataset[Row] = df1.groupBy("GISJOIN", "year").max("temp")
+
+    //val collected: Array[Row] = ds2.collect()
+    //for ()
+
+
+
+
+    //val df3: DataFrame = ds2.where(ds2("GISJOIN") === "G1200170").sort("year")
 
     println("Exiting!")
 
