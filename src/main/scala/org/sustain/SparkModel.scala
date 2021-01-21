eeb44c82ae922925769f0bfc64278b48934e5edb
Caleb Carlson <ccarlson355@gmail.com>
2021-01-21 15:41:51 -0700

    feat(core): Test13

--- src/main/scala/org/sustain/SparkModel.scala
+++ src/main/scala/org/sustain/SparkModel.scala
@@ -76,7 +76,48 @@ object SparkModel {
     for (gisJoinRow: Row <- gisJoins.collect()) {
       val gisJoin: String = gisJoinRow.getString(0)
 
-      println(gisJoin)
+      // Discard all rows not for this GISJOIN, group by the year, selecting the maximum temp among the temps for that year.
+      // Finally, sort by the years, so it's in chronological order.
+      val gisDf: DataFrame = df.filter($"GISJOIN" === gisJoin).groupBy("year").max("temp").sort("year")
+
+
+      // Make a linear model for the max temps
+      // Create a feature transformer that merges multiple columns into a vector column
+      val assembler: VectorAssembler = new VectorAssembler()
+        .setInputCols(Array("year"))
+        .setOutputCol("max(temp)")
+
+      // Merge multiple feature columns into a single vector column
+      val mergedDf = assembler.transform(gisDf)
+
+      // Split input into testing set and training set:
+      // 80% training, 20% testing, with random seed of 42
+      val Array(train, test): Array[Dataset[Row]] = gisDf.randomSplit(Array(0.8, 0.2), 42)
+
+      // Create a linear regression model object and fit it to the training set
+      val linearRegression: LinearRegression = new LinearRegression()
+        .setMaxIter(10)
+        .setRegParam(0.3)
+        .setElasticNetParam(0.8)
+
+      val lrModel: LinearRegressionModel = linearRegression.fit(train)
+
+      // Print the coefficients and intercept for linear regression
+
+      println(s">>> GISJOIN ${gisJoin} RESULTS:")
+      println(s">>> Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
+
+      val trainingSummary = lrModel.summary
+      println(s"\tnumIterations: ${trainingSummary.totalIterations}")
+      println(s"\tobjectiveHistory: [${trainingSummary.objectiveHistory.mkString(",")}]")
+      trainingSummary.residuals.show()
+      println(s"\tRMSE: ${trainingSummary.rootMeanSquaredError}")
+      println(s"\tr2: ${trainingSummary.r2}")
+
+      // Use the model on the testing set, and evaluate results
+      val lrPredictions: DataFrame = lrModel.transform(test)
+      val evaluator: RegressionEvaluator = new RegressionEvaluator().setMetricName("rmse")
+      println(s"\tTest RMSE: ${evaluator.evaluate(lrPredictions)}")
     }
 
     // Group by GISJOIN and year, selecting the max of year
@@ -85,13 +126,9 @@ object SparkModel {
     //val collected: Array[Row] = ds2.collect()
     //for ()
 
-
-
-
     //val df3: DataFrame = ds2.where(ds2("GISJOIN") === "G1200170").sort("year")
 
     println("Exiting!")
-
   }
 
   // Processes arguments passed into the main function of the JAR
