a5d142aa9ff78fe2296254e300d34af087b6b097
Caleb Carlson <ccarlson355@gmail.com>
2021-03-26 09:34:39 -0600

    feature(core): Update mongo-java-driver 3.12.8

--- src/main/java/org/sustain/server/SustainServer.java
+++ src/main/java/org/sustain/server/SustainServer.java
@@ -19,7 +19,7 @@ public class SustainServer {
         "build/libs/spark-mllib_2.12-3.0.1.jar",
         "build/libs/spark-sql_2.12-3.0.1.jar",
         "build/libs/bson-4.0.5.jar",
-        "build/libs/mongo-java-driver-3.12.5.jar"
+        "build/libs/mongo-java-driver-3.12.8.jar"
     };
 
     private Server server;
